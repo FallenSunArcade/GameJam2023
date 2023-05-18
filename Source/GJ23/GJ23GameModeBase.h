ad2668f017995c7322d23c7e2ab7a19c1a15fa35
Tim <retrodinosaur1@gmail.com>
2023-05-18 05:02:47 -0600

    adding blank project

---
+++ Source/GJ23/GJ23GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GJ23GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GJ23_API AGJ23GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
